4c22b02c7b26e5d958f44312610d0715e500ab77
LeeVaughn <dlvprogramming@gmail.com>
2019-12-06 15:27:23 -0800

    required Book model and handler function to wrap routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const Book = require("../models").Book;
+
+// handler function to wrap each route
+function asyncHandler(cb) {
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', asyncHandler(async (req, res) => {
   res.send('respond with a resource');
-});
+}));
 
 module.exports = router;
