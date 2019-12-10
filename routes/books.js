0185a4bffd36888ec68af79a40067caec4295c71
LeeVaughn <dlvprogramming@gmail.com>
2019-12-10 15:43:18 -0800

    updated routing to reflect proper path, finsihed /new get and post routes

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const Book = require("../models").Book;
 
 // handler function to wrap each route
@@ -13,38 +13,34 @@ function asyncHandler(cb) {
   }
 }
 
-// home route, redirects to /books
+// shows the full list of books
 router.get("/", asyncHandler(async (req, res) => {
   res.render('index', { title: 'Books' });
 }));
 
-// shows the full list of books
-router.get("/books", asyncHandler(async (req, res) => {
-  res.render('index', { title: 'Books' });
-}));
-
 // shows the create new book form
-router.get("/books/new", asyncHandler(async (req, res) => {
-  res.send("respond with a resource");
+router.get("/new", asyncHandler(async (req, res) => {
+  res.render("new-book", { book: {}, title: "New Book" });
 }));
 
 // posts a new book to the database
-router.post("/books/new", asyncHandler(async (req, res) => {
-  res.send("respond with a resource");
+router.post("/new", asyncHandler(async (req, res) => {
+  const book = await Book.create(req.body);
+  res.redirect("/books/" + book.id);
 }));
 
 // shows the book detail form
-router.get("/books/:id", asyncHandler(async (req, res) => {
+router.get("/:id", asyncHandler(async (req, res) => {
   res.send("respond with a resource");
 }));
 
 // updates book info to the database
-router.post("/books/:id", asyncHandler(async (req, res) => {
+router.post("/:id", asyncHandler(async (req, res) => {
   res.send("respond with a resource");
 }));
 
 // deletes a book from the database
-router.post("/books/:id/delete", asyncHandler(async (req, res) => {
+router.post("/:id/delete", asyncHandler(async (req, res) => {
   res.send("respond with a resource");
 }));
 
